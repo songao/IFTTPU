61144db8efa7cc0f35654b7cd42695a43aa5dc97
Ao Song <songao.baidu@gmail.com>
2011-11-12 16:53:07 +0800

    almost finish

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ $(function() {
     ifttpu.navigator.render($('#nav')[0], ifttpu.config.navigator, '首页');
     
     //图片轮播
-    var $o = $(".top-slide");
-    var ks = $o.find("ol").Oslide({
-        slidetag:$o.find("ol>li"),
-        easing:"easeInOutCirc",
-        direct:'down',
-        time:6000,
-        speed:450
-    });
+    var $o = $(".top-slide"),
+        ks = $o.find("ol").Oslide({
+            slidetag:$o.find("ol>li"),
+            easing:"easeInOutCirc",
+            direct:'up',
+            time:6000,
+            speed:450
+        });
 });
